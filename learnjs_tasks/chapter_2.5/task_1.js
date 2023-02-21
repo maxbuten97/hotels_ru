e6b4aafffba4951d8129fe5fc192b52c5d3af66b
Maxim <maxbuten97@gmail.com>
2023-02-21 18:26:20 +0300

    done site BEAUTY and CONQUEST,  learnJS up to chapter 2.13, done htmlAcademy and video youTube HTML CSS 4-24, 30-34 lessons

---
+++ learnjs_tasks/chapter_2.5/task_1.js
@@ -0,0 +1,20 @@
+'use strict';
+/*
+Шаблонные строки
+
+Что выведет этот скрипт?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
+
+
+Ответ: 
+hello 1
+hello "name"
+hello Ilya
+*/
